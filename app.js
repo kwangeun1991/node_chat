93d1fe4ab0f2db9f60a44edc008df0bd9ee126b1
kwangeun1991 <aosjtnstn7@naver.com>
2021-06-09 14:22:29 +0900

    socket.io room 만들기까지 완료, 대화기록 나옴

--- app.js
+++ app.js
@@ -7,11 +7,43 @@ const path = require('path');
 const server = require('http').createServer(app);////////////////////////////////////
 const io = require('socket.io')(server);////////////////////////////////////////////////
 
+// 채팅 기록 모아두기
+const chatHistory = {};
+
 io.on('connection', (socket) => {
   // socket.on -> 데이터 수신, socket.emit = 데이터 전송
   socket.on('chat', (arg) => {
-    console.log("전송받은 데이터", arg);
-    io.emit('chat', arg);
+    //console.log("전송받은 데이터", arg);
+
+    socket.sessionId = socket.sessionId || arg.sessionId;
+
+    chatHistory[socket.sessionId] = chatHistory[socket.sessionId] || [];
+    chatHistory[socket.sessionId].push(arg);
+
+    io.to(arg.room).emit('chat', arg);
+  });
+
+  // 방 참여 관련
+  socket.on('join', (room) => {
+    console.log(room + "에 참여함");
+    socket.join(room);
+  });
+
+  // 방 닫을 때 관련
+  let previousRooms = [];
+  socket.on('disconnecting', () => {
+
+  });
+
+  // 방이 닫혔을 때
+  socket.on('disconnect', () => {
+    // 방이 닫히면 현재 접속자 대회 기록 후속 처리
+    console.log(chatHistory[socket.sessionId]);
+    
+    //  후속처리
+    delete chatHistory[socket.sessionId];
+
+    console.log('disconnect');
   });
 });
 
@@ -26,6 +58,13 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
+  res.render('main');
+});
+
+app.get('/chat', (req, res, next) => {
+  if (!req.query.room || !req.query.userNm) {
+    return res.send(`<script>alert('방이름과 사용자명을 모두 입력해주세요.');location.href="/";</script>`);
+  }
   return res.render('chat');
 });
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -11,20 +11,57 @@ socket.on('chat', (arg) => {
   console.log(arg);
 });
 */
+
 const chat = {
-  room : "",
+  sessionId : "",
+  room : "", // 방이름
+  userNm : "", // 사용자 이름
+
+  /**
+  * 페이지 접속시 방이름 사용자 이름 설정
+  */
+  init : function() {
+    // location.search
+    const uid = new Date().getTime();
+    let qs = {};
+    location.search.replace("?", '')
+                    .split("&")
+                    .map((v) => {
+                      //console.log(v);
+                      v = v.split("=");
+                      v[1] = decodeURIComponent(v[1]);
+                      qs[v[0]] = v[1];
+                    });
+                    //console.log(qs);
+    this.room = qs.room || 'lobby';
+    this.userNm = qs.userNm || uid;
+    this.sessionId = "chat_" + uid;
+
+    socket.emit('join', this.room);
+  },
+
   /**
   * 소켓 서버로 메세지 전송
   *
   * @param String message 전송할 메세지
   */
-  send : function(message, userNm) {
+  send : function(message) {
     const data = {
-      userNm : userNm,
+      room : this.room,
+      sessionId : this.sessionId,
+      userNm : this.userNm,
       message : message,
     };
     socket.emit("chat", data);
   },
+  /**
+  * 채팅 메세지기 하단으로 고정 되도록 하기
+  */
+  scrollBottom : function() {
+    const $contents = $(".chat .contents");
+    $contents[0].scrollTop = $contents[0].scrollHeight;
+  },
+
 };
 
 /**
@@ -34,9 +71,22 @@ const chat = {
 socket.on('chat', (data) => {
   //console.log(data);
   let html = $("#chat_template").html();
+  let addClass = 'other';
+  if (data.userNm == chat.userNm) {
+    addClass = 'mine';
+  }
+
+  html = html.replace(/<%=addClass%>/g, addClass);
+  html = html.replace(/<%=userNm%>/g, data.userNm);
   html = html.replace(/<%=message%>/g, data.message);
+
   $(".chat .contents").append(html);
+
+  chat.scrollBottom();
 });
+
+// 페이지 접속시 초기화
+chat.init();
 
 $(function() {
   $(".chat #word").keyup(function(e) {
